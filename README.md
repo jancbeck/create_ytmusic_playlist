be6f1ff64e57f62642bbb77602375b2d87daa5f0
Jan Beck <jancbeck@users.noreply.github.com>
2023-03-17 14:25:04 +0100

    added documentation

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
-# yt-match
- Accepts a JSON file of songs and looks them up on Youtube Music
+# YouTube Music Playlist Creator
+
+This Python script creates a YouTube Music playlist from a JSON file containing song information. It searches for songs using the YouTube Music API and adds them to a new playlist.
+
+## Features
+
+- Automatically create a playlist on YouTube Music with a custom name and description.
+- Search for songs and add them to the playlist.
+- Warn about duplicate songs being added to the playlist.
+- Save the playlist ID to an output file.
+- Save any warnings to a log file.
+- Support verbose mode for printing warnings during execution.
+- Cache video IDs in case of errors to avoid losing progress.
+
+## Prerequisites
+
+- Python 3.x
+- ytmusicapi package
+
+Install the ytmusicapi package with the following command:
+
+```
+pip install ytmusicapi
+```
+
+## Usage
+
+1. Prepare a JSON file containing the song data.
+2. Create a `headers_auth.json` file for authenticating with the YouTube Music API. Follow the instructions in the [ytmusicapi](https://github.com/sigma67/ytmusicapi#setup) repository.
+3. Run the script:
+
+```
+python create_ytm_playlist.py path/to/your/json_file.json -o output_file.txt -l log_file.txt -v
+```
+
+## Arguments
+
+- `json_file`: The path to the JSON file containing song data.
+- `-o`, `--output_file`: (Optional) The path to the text file to store the playlist ID.
+- `-v`, `--verbose`: (Optional) Enable verbose mode (print warnings).
+- `-l`, `--log_file`: (Optional) The path to the log file to store warnings.
+
+## JSON File Format
+
+The JSON file should have the following format:
+
+```json
+[
+  {
+    "name": "Playlist Name",
+    "tracks": [
+      {
+        "artist": "Artist Name",
+        "track": "Track Name"
+      },
+      ...
+    ]
+  }
+]
+```
+
+Replace "Playlist Name" with your desired playlist name, and add as many artist-track pairs as needed.
+
+## License
+This project is licensed under the MIT License.
--- create_ytmusic_playlist.py
+++ create_ytmusic_playlist.py
@@ -1,26 +1,33 @@
+# Import required libraries
 import json
 import sys
 import os
 import re
 import unicodedata
+import argparse
 from datetime import datetime
 from ytmusicapi import YTMusic
 from queue import Queue
 from threading import Thread
+from requests.exceptions import HTTPError
 
+# Function to normalize strings, removing accents and converting to lowercase
 def normalize_string(text):
     normalized = unicodedata.normalize('NFD', text).encode('ASCII', 'ignore').decode('utf-8')
     return normalized.lower()
 
+# Function to tokenize a string, returning a set of unique words
 def tokenize_string(text):
     return set(re.findall(r'\b\w+\b', normalize_string(text)))
 
+# Function to compare two strings by tokenizing and finding the ratio of matched tokens
 def compare_strings(query, result):
     query_tokens = tokenize_string(query)
     result_tokens = tokenize_string(result)
     matched_tokens = query_tokens & result_tokens
     return len(matched_tokens) / len(query_tokens)
 
+# Function to prompt the user for their desired match from search results
 def prompt_user_choice(search_query, search_results):
     print(f"\nOriginal query: {search_query}")
 
@@ -48,82 +55,136 @@ def prompt_user_choice(search_query, search_results):
     else:
         return None
 
+# Main function
 def main():
-    if len(sys.argv) < 2:
-        print("Usage: python create_ytmusic_playlist.py <JSON_file>")
-        return
+    # Set up command line arguments
+    parser = argparse.ArgumentParser(description='Create a YouTube Music playlist from a JSON file.')
+    parser.add_argument('json_file', type=str, help='Path to the JSON file containing song data')
+    parser.add_argument('-o', '--output_file', type=str, default=None, help='Optional path to the text file to store the playlist ID')
+    parser.add_argument('-v', '--verbose', action='store_true', help='Enable verbose mode (print warnings)')
+    parser.add_argument('-l', '--log_file', type=str, default=None, help='Optional path to the log file to store warnings')
+    args = parser.parse_args()
 
-    json_file = sys.argv[1]  # Get JSON file name from command line
-    playlist_title = os.path.splitext(os.path.basename(json_file))[0]  # Set playlist title based on file name
-    playlist_title += f" - {datetime.now().strftime('%Y%m%d-%H%M%S')}"  # Add timestamp to playlist title
-    playlist_description = 'A playlist created from a JSON file.'  # Set your desired playlist description
+    # Read command line arguments
+    json_file = args.json_file
+    output_file = args.output_file
+    verbose = args.verbose
+    log_file = args.log_file
 
+    # Load JSON data
     with open(json_file) as f:
         data = json.load(f)
 
+    # Set playlist title and description
+    playlist_title = data[0].get('name', os.path.splitext(os.path.basename(json_file))[0])
+    playlist_title += f" - {datetime.now().strftime('%Y%m%d-%H%M%S')}"
+    playlist_description = 'A playlist created from a JSON file.'
+
+    # Load the JSON data again
+    with open(json_file) as f:
+        data = json.load(f)
+
+       # Initialize the YTMusic API with the authentication headers
     ytmusic = YTMusic('headers_auth.json')
 
-    video_ids = set()  # Initialize a set to store unique video IDs
+    # Check for cache file and load video IDs if it exists
+    cache_file = f"{os.path.splitext(json_file)[0]}_cache.json"
+    if os.path.exists(cache_file):
+        with open(cache_file, 'r') as f:
+            video_ids = set(json.load(f))
+    else:
+        video_ids = set()  # Initialize a set to store unique video IDs
     log = []  # Initialize a list to store duplicate warnings
 
-    user_input_queue = Queue()
+    if len(video_ids) == 0:
 
-    def process_user_input():
-        while True:
-            search_query, search_result = user_input_queue.get()
-            
-            video_id = prompt_user_choice(search_query, search_result)
+        user_input_queue = Queue()
 
-            if video_id is None:
-                # Perform a search with the scope set to "uploads"
-                video_results = ytmusic.search(search_query, filter='videos', ignore_spelling=True)[:10] # limit results to 10
+        # Function to process user input
+        def process_user_input():
+            while True:
+                search_query, search_result = user_input_queue.get()
 
-                if video_results and len(video_results) > 0:
-                    video_id = prompt_user_choice(search_query, video_results)
+                video_id = prompt_user_choice(search_query, search_result)
 
-            if video_id is not None:
-                if video_id in video_ids:
-                    log.append(f"Warning: Duplicate video ID ({video_id}) for {artist} - {track}")
+                if video_id is None:
+                    # Perform a search with the scope set to "uploads"
+                    video_results = ytmusic.search(search_query, filter='videos', ignore_spelling=True)[:10] # limit results to 10
+
+                    if video_results and len(video_results) > 0:
+                        video_id = prompt_user_choice(search_query, video_results)
+
+                if video_id is not None:
+                    if video_id in video_ids:
+                        log.append(f"Warning: Duplicate video ID ({video_id}) for {artist} - {track}")
+                    else:
+                        video_ids.add(video_id)  # Add the video ID to the set
+
+                user_input_queue.task_done()
+
+        # Start the user input processing thread
+        user_input_thread = Thread(target=process_user_input)
+        user_input_thread.daemon = True
+        user_input_thread.start()
+
+        # Iterate through the tracks and search for them on YouTube Music
+        for item in data[0]['tracks']:
+            artist = item['artist']
+            track = item['track']
+            search_query = f"{artist} - {track}"
+            search_results = ytmusic.search(search_query, filter='songs', ignore_spelling=True)[:10] # limit results to 10
+
+            if search_results and len(search_results) > 0:
+                first_match_ratio = compare_strings(search_query, f"{search_results[0]['artists'][0]['name']} - {search_results[0]['title']}")
+
+                if first_match_ratio < 0.5:
+                    user_input_queue.put((search_query, search_results))
                 else:
-                    video_ids.add(video_id)  # Add the video ID to the set
-                    
-            user_input_queue.task_done()
+                    video_id = search_results[0]['videoId']
 
-    # Start the user input processing thread
-    user_input_thread = Thread(target=process_user_input)
-    user_input_thread.daemon = True
-    user_input_thread.start()
-    
-    for item in data[0]['tracks']:
-        artist = item['artist']
-        track = item['track']
-        search_query = f"{artist} - {track}"
-        search_results = ytmusic.search(search_query, filter='songs', ignore_spelling=True)[:10] # limit results to 10
+                    if video_id in video_ids:
+                        log.append(f"Warning: Duplicate video ID ({video_id}) for {artist} - {track}")
+                    else:
+                        video_ids.add(video_id)  # Add the video ID to the set
 
-        if search_results and len(search_results) > 0:
-            first_match_ratio = compare_strings(search_query, f"{search_results[0]['artists'][0]['name']} - {search_results[0]['title']}")
+        # Wait for the user to finish providing input
+        user_input_queue.join()
 
-            if first_match_ratio < 0.5:
-                user_input_queue.put((search_query, search_results))
-            else:
-                video_id = search_results[0]['videoId']
+    try:
+        # Add all unique video IDs to the playlist at once
+        playlist_id = ytmusic.create_playlist(playlist_title, playlist_description, privacy_status='UNLISTED')
+        add_items_result = ytmusic.add_playlist_items(playlist_id, list(video_ids))
+        log.append(f"add_playlist_items result: {add_items_result}")
 
-                if video_id in video_ids:
-                    log.append(f"Warning: Duplicate video ID ({video_id}) for {artist} - {track}")
-                else:
-                    video_ids.add(video_id)  # Add the video ID to the set
+        # Delete cache file if it exists
+        if os.path.exists(cache_file):
+            os.remove(cache_file)
 
-    # Wait for the user to finish providing input
-    user_input_queue.join()
+        # Append the playlist ID to the specified text file, if provided
+        if output_file is not None:
+            with open(output_file, 'a') as f:
+                f.write(f"https://music.youtube.com/playlist?list={playlist_id}\n")
 
-    # Display the duplicate warnings
+    except HTTPError as e:
+        if e.response.status_code == 400:
+            print(f"Error: {e}")
+            print("Saving video IDs to cache file...")
+            with open(cache_file, 'w') as f:
+                json.dump(list(video_ids), f)
+        else:
+            raise
+
+    # Display warnings
     for warning in log:
-        print(warning)
+        if verbose and warning.startswith("Warning:"):
+            print(warning)
+    
+    # Save warnings to log file, if provided
+    if log_file is not None:
+        with open(log_file, 'a') as f:
+            for warning in log:
+                f.write(f"{warning}\n")
 
-    # Add all unique video IDs to the playlist at once
-    playlist_id = ytmusic.create_playlist(playlist_title, playlist_description, privacy_status='UNLISTED')
-    add_items_result = ytmusic.add_playlist_items(playlist_id, list(video_ids))
-    print(f"add_playlist_items result: {add_items_result}")
-
+# Run the main function
 if __name__ == "__main__":
     main()
