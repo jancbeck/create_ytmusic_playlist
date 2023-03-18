ec5f3ba443c454d32221df1f04c838e76e6244d2
Jan Beck <jancbeck@users.noreply.github.com>
2023-03-18 13:27:10 +0100

    adds numerous quality of life improvements
    - now shows a maximum of 20 results from YT music and then another max of 20 from yt videos.
    - prettier presentation of results. Song names are now clickable links in Terminal.
    - allows control of matching threshold via ´--threshold´ argument

--- create_ytmusic_playlist.py
+++ create_ytmusic_playlist.py
@@ -27,33 +27,65 @@ def compare_strings(query, result):
     matched_tokens = query_tokens & result_tokens
     return len(matched_tokens) / len(query_tokens)
 
+# Function that prints text to console as hyperlink
+def link(uri, label=None):
+    if label is None: 
+        label = uri
+    parameters = ''
+
+    # OSC 8 ; params ; URI ST <name> OSC 8 ;; ST 
+    escape_mask = '\033]8;{};{}\033\\{}\033]8;;\033\\'
+
+    return escape_mask.format(parameters, uri, label)
+
+def print_bold(text):
+    return "\033[1m" + text + "\033[0m"
+
+def print_row(col1, col2, col3):
+    formatted_string = "{:<2} {:<12} {:<80}".format(col1, col2, col3)
+    print(formatted_string)
+
 # Function to prompt the user for their desired match from search results
-def prompt_user_choice(search_query, search_results):
-    print(f"\nOriginal query: {search_query}")
+def prompt_user_choice(search_query, search_results, allow_skip=False):
 
-    for i, result in enumerate(search_results):
-        matched_artist = result['artists'][0]['name']
-        matched_title = result['title']
-        matched_query = f"{matched_artist} - {matched_title}"
-        match_ratio = compare_strings(search_query, matched_query) * 100
+    while True:
+        print(f"\nOriginal query: {print_bold(search_query)}")
+        if not search_results:
+            print("No results found.")
+            return None
 
-        print(f"{i + 1}. {matched_query} (Match: {round(match_ratio)}%)")
+        for i, result in enumerate(search_results):
+            matched_artist = result['artists'][0]['name']
+            matched_title = result['title']
+            matched_query = f"{matched_artist} - {matched_title}"
+            match_ratio = compare_strings(search_query, matched_query) * 100
+            video_url = f"https://music.youtube.com/watch?v={result['videoId']}"
+            clickable_title = link(video_url, matched_query)
 
-    user_input = input("Enter the number of the desired match to add it to the playlist, or type 'n' to skip: ")
+            print_row(i + 1, f"Match: {round(match_ratio)}%", clickable_title)
 
-    if user_input.lower() != 'n':
-        try:
-            selected_index = int(user_input) - 1
-            if 0 <= selected_index < len(search_results):
-                return search_results[selected_index]['videoId']
-            else:
+        if allow_skip:
+            prompt = "Enter the number of the desired match to add it to the playlist, or type 'n' to skip"
+        else:
+            prompt = "Enter the number of the desired match to add it to the playlist, or type 'm' for more results"
+
+        user_input = input(f"{prompt}: ")
+
+        if user_input.lower() == 'n' and allow_skip:
+            return None
+        elif user_input.lower() == 'm':
+            return None
+        else:
+            try:
+                selected_index = int(user_input) - 1
+                if 0 <= selected_index < len(search_results):
+                    return search_results[selected_index]['videoId']
+                else:
+                    print("Invalid input. Skipping this song.")
+                    return None
+            except ValueError:
                 print("Invalid input. Skipping this song.")
                 return None
-        except ValueError:
-            print("Invalid input. Skipping this song.")
-            return None
-    else:
-        return None
 
 # Main function
 def main():
@@ -64,6 +96,7 @@ def main():
     parser.add_argument('-o', '--output_file', type=str, default=None, help='Optional path to the text file to store the playlist ID')
     parser.add_argument('-v', '--verbose', action='store_true', help='Enable verbose mode (print warnings)')
     parser.add_argument('-l', '--log_file', type=str, default=None, help='Optional path to the log file to store warnings')
+    parser.add_argument('--threshold', type=float, default=0.5, help='Matching ratio threshold (default: 0.5)')
     args = parser.parse_args()
 
     # Read command line arguments
@@ -72,6 +105,7 @@ def main():
     output_file = args.output_file
     verbose = args.verbose
     log_file = args.log_file
+    match_threshold = args.threshold
 
     # Load JSON data
     with open(json_file) as f:
@@ -90,7 +124,7 @@ def main():
     ytmusic = YTMusic(auth_file)
 
     # Check for cache file and load video IDs if it exists
-    cache_file = f"{os.path.splitext(json_file)[0]}_cache.json"
+    cache_file = f"{os.path.splitext(json_file)[0]}.cached"
     if os.path.exists(cache_file):
         with open(cache_file, 'r') as f:
             video_ids = set(json.load(f))
@@ -107,14 +141,12 @@ def main():
             while True:
                 search_query, search_result = user_input_queue.get()
 
-                video_id = prompt_user_choice(search_query, search_result)
+                video_id = prompt_user_choice(search_query, search_result, allow_skip=False)
 
                 if video_id is None:
                     # Perform a search with the scope set to "uploads"
-                    video_results = ytmusic.search(search_query, filter='videos', ignore_spelling=True)[:10] # limit results to 10
-
-                    if video_results and len(video_results) > 0:
-                        video_id = prompt_user_choice(search_query, video_results)
+                    video_results = ytmusic.search(search_query, filter='videos', ignore_spelling=True) 
+                    video_id = prompt_user_choice(search_query, video_results, allow_skip=True)
 
                 if video_id is not None:
                     if video_id in video_ids:
@@ -134,12 +166,12 @@ def main():
             artist = item['artist']
             track = item['track']
             search_query = f"{artist} - {track}"
-            search_results = ytmusic.search(search_query, filter='songs', ignore_spelling=True)[:10] # limit results to 10
+            search_results = ytmusic.search(search_query, filter='songs', ignore_spelling=True)
 
             if search_results and len(search_results) > 0:
                 first_match_ratio = compare_strings(search_query, f"{search_results[0]['artists'][0]['name']} - {search_results[0]['title']}")
 
-                if first_match_ratio < 0.5:
+                if first_match_ratio < match_threshold:
                     user_input_queue.put((search_query, search_results))
                 else:
                     video_id = search_results[0]['videoId']
